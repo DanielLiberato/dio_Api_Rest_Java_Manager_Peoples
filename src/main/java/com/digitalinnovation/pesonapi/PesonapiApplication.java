05bcf67613341826d2bfe92ae4324d2bf413acaf
Daniel Liberato da Silva <daniel.bacellar022@gmail.com>
2022-01-17 19:28:28 -0400

    create project

---
+++ src/main/java/com/digitalinnovation/pesonapi/PesonapiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.pesonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation/pesonapi/PesonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.pesonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
